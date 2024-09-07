bf266a03c9b1f535751d9eb902a2c093387f69a2
Sandro Luiz <91218259+Saandroo@users.noreply.github.com>
2024-09-06 23:23:01 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+function pesquisar() {
+    // Obtém a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+    console.log(section);
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+    
+    if(campoPesquisa == ""){
+      section.innerHTML = "<p><b>Digite o texto para carregar a pesquisa.<b></p>"
+      return
+    }
+    campoPesquisa = campoPesquisa.toLowerCase()
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+    let titulo = "";
+    let palavra = "";
+    let tags = "";
+  
+    
+    
+  
+    // Itera sobre os dados da pesquisa e cria o HTML para cada resultado
+    for (let dado of dados) {
+      titulo = dado.titulo.toLowerCase()
+      palavra = dado.palavra.toLowerCase()
+      tags = dado.tags.toLowerCase()
+     
+      if (titulo.includes(campoPesquisa) || palavra.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+        resultados += `
+        <div class="item-resultado">
+          <h2>${dado.titulo} ${dado.capitulo}: ${dado.versiculo}</h2>
+          <p class="descricao-meta">
+            <b>${dado.palavra}</b>
+          </p>
+          <div class="mais-info">
+            <a href=${dado.link} target="_blank"><b>Estudo completo</b></a>
+          </div>
+        </div>
+      `;
+        
+      }
+  
+      // Cria um novo elemento div para cada resultado
+      
+    }
+    
+    if(!resultados) {
+      resultados = "<p><b>Nada foi encontrado<b></p>"
+
+    }
+  
+    // Atribui o HTML gerado à seção de resultados
+    section.innerHTML = resultados;
+  }
+
+
+
+
+
---
+++ dados.js
@@ -0,0 +1,115 @@
+let dados = [
+    {
+        titulo: "João",
+        capitulo: 3,
+        versiculo: 16,
+        palavra: "Porque Deus amou o mundo de tal maneira, que deu o seu Filho Unigênito, para que todo aquele que nele crê não pereça, mas tenha a vida eterna",
+        link: "https://jesusdiario.com.br/joao-3-16-estudo/",
+        tags: "joao Joao joao316 3 joao3:16 joao3,16 3:16",
+    
+    },
+
+    {
+        titulo: "Salmos",
+        capitulo: 23,
+        versiculo: 1,
+        palavra: "O Senhor é o meu pastor, nada me faltará.",
+        link: "https://bibliotecadopregador.com.br/salmos-23-estudo-esboco/",
+        tags:"faltara sl salmos23:1 23:1 23,1 23 salmos23,1",
+    },
+
+    {
+        titulo: "Romanos",
+        capitulo: 12,
+        versiculo: 2,
+        palavra: "E não se amoldem ao padrão deste mundo, mas transformem-se pela renovação da sua mente, para que provem qual seja a boa, agradável e perfeita vontade de Deus.",
+        link: "https://jesusdiario.com.br/romanos-12-2/",
+        tags: "renovacao renovaçao agradavel rm romanos12:2 12 romanos12,2 12,2 12:2",
+    },
+
+    {
+        titulo: "Mateus",
+        capitulo: 5,
+        versiculo: 16,
+        palavra: "Assim brilhe a vossa luz diante dos homens, para que vejam as vossas boas obras e glorifiquem a vosso Pai que está nos céus.",
+        tags: "mt mateus5:16 mateus5,16 mt5:16 mt5,16 5,16 5 5:16" ,
+        link: "https://versiculoscomentados.com.br/index.php/estudo-de-mateus-5-16-comentado-e-explicado/",
+    },
+
+    {
+        titulo: "Marcos",
+        capitulo: 12,
+        versiculo: 31,
+        palavra: "E amando ao próximo como a si mesmo, cumpre toda a Lei.",
+        link: "https://www.apologeta.com.br/marcos-12-31/",
+        tags: "mc marcos12,31 12 marcos12:31 mc12:31 mc12,31 12,31 12:31",
+    },
+
+    {
+        titulo: "João",
+        capitulo: 14,
+        versiculo: 6,
+        palavra: "Jesus lhe disse: Eu sou o caminho, e a verdade, e a vida; ninguém vem ao Pai, senão por mim.",
+        link: "https://jesusdiario.com.br/eu-sou-o-caminho-a-verdade-e-a-vida/",
+        tags:"jaoa Joao senao jo joao14:6 14,6 joao14,6 14",
+    },
+
+    {
+        titulo: "Gênesis",
+        capitulo: 1,
+        versiculo: 1,
+        palavra: "No princípio, Deus criou os céus e a terra.",
+        link: "https://estiloadoracao.com/genesis-1-estudo/",
+        tags: "criacao deus  ceu  terra principio genesis genesis1,1 genesis1:1 genesis1 gn ",
+    },
+
+    {
+      titulo: "Êxodo",
+      capitulo: 3,
+      versiculo: 14,
+      palavra: "E Deus disse a Moisés: EU SOU O QUE SOU. E disse mais: Assim dirás aos filhos de Israel: EU SOU me enviou a vós.",
+      link: "https://www.apologeta.com.br/exodo-3-14/",
+       tags: " exodo exodo3:14 exodo3,14 3:14 3,14 ex",
+    },
+
+    {
+      titulo: "Levítico",
+      capitulo: 11,
+      versiculo: 1,
+      palavra: "E o Senhor falou a Moisés e a Arão, dizendo: Fala aos filhos de Israel, dizendo: Estes são os animais que podeis comer de todos os animais que há sobre a terra.",
+      link: "https://jesuseabiblia.com/biblia-de-estudo-online/levitico-11-estudo/",
+      tags: "alimentos levitico levitico11,1 levitico11:1 11,1 11;1 lv puros impuros leis_alimentícias",
+    },
+
+    {
+      titulo: "Números",
+      capitulo: 11,
+      versiculo: 6,
+      palavra: "E Moisés disse ao Senhor: O povo, entre o qual habito, é seiscentos mil homens de pé; e tu dizes: Dar-lhes-ei carne, para que comam um mês inteiro? Haverá tanto gado nas pastagens para lhes dar carne, ou se ajuntarão para nós todos os peixes do mar, para lhes dar de comer?",
+      link: "https://www.apologeta.com.br/numeros-11-6/d",
+      tags: "mana carne numero moises numeros11,6 numeros11:6 11,6 11:6 numeros",
+    },
+
+    {
+      titulo: "Deuteronômio",
+      capitulo: 6,
+      versiculo: 4,
+      palavra: "mas vocês, que permaneceram fiéis ao Senhor, o Deus de vocês, estão hoje todos vivos.",
+      link: "https://www.apologeta.com.br/deuteronomio-6-4/",
+      tags: "amor deuteronomio dt dt6,4 dt6:4 deuteronomio6,4 deuteronomio6:4 6:4 6,4 deus coração alma forças",
+    },
+
+    {
+      titulo: "Josué",
+      capitulo: 1,
+      versiculo: 9,
+      palavra: "Não fui eu que ordenei a você? Seja forte e corajoso! Não se apavore nem desanime, pois o Senhor, o seu Deus, estará com você por onde você andar",
+      link: "https://jesuseabiblia.com/biblia-de-estudo-online/josue-1-estudo/",
+      tags: "josue js josue1,9 josue1:9 js1,9 js1:9 1:9 1,9",
+    },
+    {
+
+    },
+
+];
+
